fc9fadf7b897d0268f5e1f69dea740806df80e97
randilt <randiltharusha72@gmail.com>
2023-10-15 21:37:06 +0530

    Create object pool

--- script.js
+++ script.js
@@ -19,9 +19,40 @@ class Player {
     else if (this.x > this.game.width - this.width)
       this.x = this.game.width - this.width;
   }
+  shoot() {
+    const projectile = this.game.getProjectile();
+    if (projectile) projectile.start(this.x, this.y);
+  }
 }
 
-class Projectile {}
+class Projectile {
+  constructor() {
+    this.width = 4;
+    this.height = 20;
+    this.x = 0;
+    this.y = 0;
+    this.speed = 20;
+    this.free = true;
+  }
+  draw(context) {
+    if (!this.free) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+  update() {
+    if (!this.free) {
+      this.y -= this.speed;
+    }
+  }
+  start(x, y) {
+    this.x = x;
+    this.y = y;
+    this.free = false;
+  }
+  reset() {
+    this.free = true;
+  }
+}
 
 class Enemy {}
 
@@ -33,11 +64,17 @@ class Game {
     this.keys = []; // empty array to store the keys pressed for player movement
     this.player = new Player(this);
 
+    this.projectilesPool = [];
+    this.numberOfProjectiles = 10;
+    this.createProjectiles();
+    console.log(this.projectilesPool);
+
     //EVENT LISTENERS
 
     //this will push the recently pressed key to the this.keys array only if the key is not already present in the array.
     window.addEventListener("keydown", (e) => {
       if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+      if (e.key === "1") this.player.shoot();
     });
     //this will remove the added key from this.keys array on key release
     window.addEventListener("keyup", (e) => {
@@ -48,6 +85,22 @@ class Game {
   render(context) {
     this.player.draw(context);
     this.player.update();
+    this.projectilesPool.forEach((projectile) => {
+      projectile.update();
+      projectile.draw(context);
+    });
+  }
+  // create projectiles object pool
+  createProjectiles() {
+    for (let i = 0; i < this.numberOfProjectiles; i++) {
+      this.projectilesPool.push(new Projectile());
+    }
+  }
+  //get free projectile object
+  getProjectile() {
+    for (let i = 0; i < this.projectilesPool.length; i++) {
+      if (this.projectilesPool[i].free) return this.projectilesPool[i];
+    }
   }
 }
 
